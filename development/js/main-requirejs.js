eabe9544a66a768a5bfe90745b03b41dcfa31330
Daiwei Lu <daiweilu123@gmail.com>
2013-08-24 20:28:24 -0400

    Switch api key to production

--- development/js/main-requirejs.js
+++ development/js/main-requirejs.js
@@ -12,7 +12,7 @@ require.config({
       exports: 'FB',
       init: function() {
         this.FB.init({
-          appId      : '580227458695144',
+          appId      : '153060941567545',
           channelUrl : location.origin + '/fb_channel.html',
           status     : true,
           cookie     : true,
