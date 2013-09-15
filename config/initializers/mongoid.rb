6417c82f565284a3af3be5d2d6377ecad127e6b3
Daiwei Lu <daiweilu123@gmail.com>
2013-09-15 14:28:32 -0400

    Convert ISO 8601 time into unix time when Restangularize object
    - Convert c_at, u_at to created_at, updated_at when convert MongoID object to JSON

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -4,6 +4,10 @@ module Mongoid
     def as_json(options={})
       attrs = super(options)
       attrs["id"] = self.id.to_s
+      attrs["created_at"] = self.c_at
+      attrs["updated_at"] = self.u_at
+      attrs.delete('c_at')
+      attrs.delete('u_at')
       attrs
     end
   end
