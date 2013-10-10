37c0bba86f41318e0943a5af1f7bc24513555288
Daiwei Lu <daiweilu123@gmail.com>
2013-10-09 23:15:34 -0400

    RSpec testing for fb_login

--- spec/controllers/auth_controller_fb_login_spec.rb
+++ spec/controllers/auth_controller_fb_login_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe Api::AuthController do
-  describe 'GET fb_register' do
+  describe 'GET fb_login' do
     it 'should return token code and user data' do
-      user = FactoryGirl.build(:user_with_facebook_login)
-      post :fb_register, {user: {
+      user = FactoryGirl.create(:user_with_facebook_login)
+      post :fb_login, {user: {
         fb_access_token: user.fb_access_token,
         fb_user_id:      user.fb_user_id,
         name:            user.name,
@@ -18,9 +18,20 @@ describe Api::AuthController do
       expect(response.cookies['user_id']).to eq(session[:user_id].to_s)
     end
 
-    it 'should return 406' do
+    it 'should return 406 because user not exist' do
       user = FactoryGirl.build(:user_with_facebook_login)
-      post :fb_register, {user: {
+      post :fb_login, {user: {
+        fb_access_token: 1234567890,
+        fb_user_id:      user.fb_user_id,
+        name:            user.name,
+        email:           user.email
+      }}
+      expect(response.status).to eq(406)
+    end
+
+    it 'should return 406 because user not exist' do
+      user = FactoryGirl.create(:user_with_facebook_login)
+      post :fb_login, {user: {
         fb_access_token: 1234567890,
         fb_user_id:      user.fb_user_id,
         name:            user.name,
