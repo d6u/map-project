5e74297503571e9a1252d6b700b982d0cd5881fd
Daiwei Lu <daiweilu123@gmail.com>
2013-10-15 14:31:37 -0400

    Fix a bug of last commit

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -9,13 +9,12 @@ $redis = Redis.new
 puts "--> Caching project participating_user ids on Redis"
 
 # clean up old caches
-$redis.del( $redis.keys("project:*:user_ids") )
+old_keys = $redis.keys("project:*:user_ids")
+$redis.del(old_keys) if !old_keys.empty?
 
 # cache new sets
 Project.all.each do |project|
   puts "--> Caching project #{project.id}"
   ids = project.participating_users.pluck(:id) + [project.owner_id]
-  if !ids.empty?
-    $redis.sadd("project:#{project.id}:user_ids", ids)
-  end
+  $redis.sadd("project:#{project.id}:user_ids", ids) if !ids.empty?
 end
